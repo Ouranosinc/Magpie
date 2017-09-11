c82653d01293506314717a23f22dc73f4987b791
David Byrns <david.byrns@crim.ca>
2017-09-11 11:57:05 -0400

    Fix for include path

--- magpie/ui/home/views.py
+++ magpie/ui/home/views.py
@@ -1,6 +1,7 @@
 import requests
 from pyramid.view import view_config
-from home import add_template_data
+from ui.home import add_template_data
+
 
 @view_config(route_name='home', renderer='templates/home.mako')
 def home_view(request):
