2c27bf5ab00fdfd4f4ace518849032195d426d72
Francis Charette Migneault <francis.charette-migneault@crim.ca>
2017-11-10 15:23:11 -0500

    test services auto-register

--- magpie/register.py
+++ magpie/register.py
@@ -4,15 +4,16 @@ import yaml
 
 LOGIN_ATTEMPT = 10
 LOGIN_TIMEOUT = 10
+LOGIN_TMP_DIR = "~/tmp"
 
 
-def login_loop(login_url, cookies, admin_name, admin_password, extra_data=''):
+def login_loop(login_url, cookies, admin_name, admin_password, extra_data='', message='Login response'):
     extra_data = '&' + str(extra_data) if extra_data is not None else ''
     params = '--cookie-jar {0} --data "user_name={1}&password={2}{3}"' \
-        .format(cookies, admin_name, admin_password, extra_data)
+             .format(cookies, admin_name, admin_password, extra_data)
     attempt = 0
     while True:
-        if request_curl(login_url, params, 'Login response'):
+        if request_curl(login_url, params, message):
             break
         time.sleep(LOGIN_TIMEOUT)
         attempt += 1
@@ -29,15 +30,19 @@ def phoenix_login(cookies):
     try:
         phoenix_usr = os.getenv('PHOENIX_USER')
         phoenix_pwd = os.getenv('PHOENIX_PASSWORD')
+        if phoenix_usr is None:
+            raise ValueError("Environment variable was None", 'PHOENIX_USER')
+        if phoenix_pwd is None:
+            raise ValueError("Environment variable was None", 'PHOENIX_PASSWORD')
     except Exception as e:
         raise Exception("Missing environment values [" + repr(e) + "]")
     phoenix_url = get_phoenix_url()
     login_url = phoenix_url + '/account/login/phoenix'
-    login_loop(login_url, cookies, phoenix_usr, phoenix_pwd, 'submit=submit')
+    login_loop(login_url, cookies, phoenix_usr, phoenix_pwd, 'submit=submit', 'Phoenix login response')
 
 
 def phoenix_remove_services():
-    phoenix_cookies = '/tmp/login_cookie_phoenix'
+    phoenix_cookies = os.path.join(LOGIN_TMP_DIR, 'login_cookie_phoenix')
     phoenix_login(phoenix_cookies)
 
     phoenix_url = get_phoenix_url()
@@ -49,7 +54,7 @@ def phoenix_remove_services():
 
 
 def phoenix_register_services(services_dict):
-    phoenix_cookies = '/tmp/login_cookie_phoenix'
+    phoenix_cookies = os.path.join(LOGIN_TMP_DIR, 'login_cookie_phoenix')
     phoenix_login(phoenix_cookies)
 
     # Register WPS services
@@ -57,7 +62,7 @@ def phoenix_register_services(services_dict):
     register_service_url = phoenix_url + '/services/register'
     wps_services_dict = [wps_service for wps_service in services_dict
                          if str(wps_service.get('type')).lower() == 'wps']
-    success = register_services(register_service_url, wps_services_dict, phoenix_cookies)
+    success = register_services(register_service_url, wps_services_dict, phoenix_cookies, 'Phoenix register service')
 
     os.remove(phoenix_cookies)
     return success
@@ -67,6 +72,10 @@ def get_phoenix_url():
     try:
         hostname = os.getenv('HOSTNAME')
         phoenix_port = os.getenv('PHOENIX_PORT')
+        if hostname is None:
+            raise ValueError("Environment variable was None", 'HOSTNAME')
+        if phoenix_port is None:
+            raise ValueError("Environment variable was None", 'PHOENIX_PORT')
     except Exception as e:
         raise Exception("Missing environment values [" + repr(e) + "]")
     return 'https://{0}:{1}'.format(hostname, phoenix_port)
@@ -76,12 +85,16 @@ def get_magpie_url():
     try:
         hostname = os.getenv('HOSTNAME')
         magpie_port = os.getenv('MAGPIE_PORT')
+        if hostname is None:
+            raise ValueError("Environment variable was None", 'HOSTNAME')
+        if magpie_port is None:
+            raise ValueError("Environment variable was None", 'MAGPIE_PORT')
     except Exception as e:
         raise Exception("Missing environment values [" + repr(e) + "]")
     return 'http://{0}:{1}'.format(hostname, magpie_port)
 
 
-def register_services(register_service_url, services_dict, cookies):
+def register_services(register_service_url, services_dict, cookies, message='Register response'):
     success = True
     for service in services_dict:
         cfg = services_dict[service]
@@ -97,32 +110,36 @@ def register_services(register_service_url, services_dict, cookies):
                  'service_type={cfg[type]}&'    \
                  'register=register"'           \
                  .format(cookie=cookies, name=service, url=url, public=public, cfg=cfg)
-        success = success and request_curl(register_service_url, params, 'Register response')
+        success = success and request_curl(register_service_url, params, message)
     return success
 
 
 def magpie_register_services(service_config_file_path):
     try:
-        admin_name = os.getenv('ADMIN_USER')
-        admin_password = os.getenv('ADMIN_PASSWORD')
+        admin_usr = os.getenv('ADMIN_USER')
+        admin_pwd = os.getenv('ADMIN_PASSWORD')
+        if admin_usr is None:
+            raise ValueError("Environment variable was None", 'ADMIN_USER')
+        if admin_pwd is None:
+            raise ValueError("Environment variable was None", 'ADMIN_PASSWORD')
     except Exception as e:
         raise Exception("Missing environment values [" + repr(e) + "]")
 
     try:
         services_cfg = yaml.load(open(service_config_file_path, 'r'))
-        services = services_cfg['services']
+        services = services_cfg['providers']
     except Exception as e:
         raise Exception("Bad service file + [" + repr(e) + "]")
     magpie_url = get_magpie_url()
 
     # Need to login first as admin
     login_url = magpie_url + '/signin'
-    cookie_fn = '/tmp/login_cookie_magpie'
-    login_loop(login_url, cookie_fn, admin_name, admin_password, 'provider_name=ziggurat')
+    cookie_fn = os.path.join(LOGIN_TMP_DIR, 'login_cookie_magpie')
+    login_loop(login_url, cookie_fn, admin_usr, admin_pwd, 'provider_name=ziggurat', 'Magpie login response')
 
     # Register services
     register_service_url = magpie_url + '/services'
-    success = register_services(register_service_url, services, cookie_fn)
+    success = register_services(register_service_url, services, cookie_fn, 'Magpie register service')
 
     os.remove(cookie_fn)
     return success
