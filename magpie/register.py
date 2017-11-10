00e2a82598ffd48dc2affab9ad60bd2a2363b3e3
Francis Charette Migneault <francis.charette-migneault@crim.ca>
2017-11-10 16:36:22 -0500

    add delay register services + push update phoenix

--- magpie/register.py
+++ magpie/register.py
@@ -3,9 +3,10 @@ import time
 import yaml
 from distutils.dir_util import mkpath
 
-LOGIN_ATTEMPT = 10
-LOGIN_TIMEOUT = 10
-LOGIN_TMP_DIR = "/tmp"
+LOGIN_ATTEMPT = 10              # max attempts for login
+LOGIN_TIMEOUT = 10              # delay (s) between each login attempt
+LOGIN_TMP_DIR = "/tmp"          # where to temporarily store login cookies
+CREATE_SERVICE_INTERVAL = 2     # delay (s) between creations to allow server to respond/process
 
 
 # alternative to 'makedirs' with 'exists_ok' parameter only available for python>3.5
@@ -122,10 +123,11 @@ def register_services(register_service_url, services_dict, cookies, message='Reg
                  'register=register"'           \
                  .format(cookie=cookies, name=service, url=url, public=public, cfg=cfg)
         success = success and request_curl(register_service_url, params, message)
+        time.sleep(CREATE_SERVICE_INTERVAL)
     return success
 
 
-def magpie_register_services(service_config_file_path):
+def magpie_register_services(service_config_file_path, push_to_phoenix=False):
     try:
         admin_usr = os.getenv('ADMIN_USER')
         admin_pwd = os.getenv('ADMIN_PASSWORD')
@@ -152,6 +154,11 @@ def magpie_register_services(service_config_file_path):
     # Magpie will not overwrite existing services by default, 409 Conflict instead of 201 Created
     register_service_url = magpie_url + '/services'
     success = register_services(register_service_url, services, magpie_cookies, 'Magpie register service')
+
+    # Push updated services to Phoenix
+    if push_to_phoenix:
+        phoenix_remove_services()
+        phoenix_register_services()
 
     os.remove(magpie_cookies)
     return success
--- register_providers.py
+++ register_providers.py
@@ -4,9 +4,15 @@ from os import path as p
 
 
 if __name__ == "__main__":
+    """
+    >> python register_providers [<path_to_config_file> [<push_to_phoenix>(0|1)]]
+    """
     if len(sys.argv) < 2:
         config_file_path = p.join(p.dirname(p.abspath(__file__)), "providers.cfg")
         print("Using default file [" + config_file_path + "] since not provided as input")
     else:
         config_file_path = sys.argv[1]
+
+    push_to_phoenix = bool(sys.argv[2]) if len(sys.argv) > 2 else False
+
     magpie_register_services(config_file_path)
