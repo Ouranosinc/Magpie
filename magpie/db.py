94d86cc4ea253e53f372119686629fc3ff595ccb
David Byrns <david.byrns@crim.ca>
2018-01-31 10:44:00 -0500

    version 0.3.2 and fix missing custom port in db url

--- magpie/db.py
+++ magpie/db.py
@@ -23,6 +23,7 @@ def get_engine(settings, prefix='sqlalchemy.'):
                    + os.getenv('POSTGRES_USER') + \
                    ':' + os.getenv('POSTGRES_PASSWORD') + \
                    '@' + os.getenv('POSTGRES_HOST') + \
+                   ':' + os.getenv('POSTGRES_PORT') + \
                    '/' + os.getenv('POSTGRES_DB')
 
    settings[prefix+'url'] = database_url
