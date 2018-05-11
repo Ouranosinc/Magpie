8d588e8a96367d83760341764a157ece76ae4c75
fmigneault <francis.charette-migneault@crim.ca>
2018-05-11 10:46:27 -0400

    revert alembic delete with proper chmod

---
+++ magpie/alembic/versions/0974132183ad_alter_user.py
@@ -0,0 +1,26 @@
+"""alter user
+
+Revision ID: 0974132183ad
+Revises: 
+Create Date: 2017-07-21 18:40:24.918345
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0974132183ad'
+down_revision = None
+branch_labels = None
+depends_on = '2bb1ba973f0b'
+
+
+def upgrade():
+    op.add_column('users', sa.Column('openid', sa.Unicode(128), unique=True))
+    op.add_column('users', sa.Column('credential', sa.Unicode(128), unique=True))
+    op.add_column('users', sa.Column('cert_expires', sa.TIMESTAMP(timezone=False),default=sa.sql.func.now(),server_default=sa.func.now()))
+
+def downgrade():
+    pass
+
---
+++ magpie/alembic/versions/54d08f9adc8c_add_registered_date_.py
@@ -0,0 +1,34 @@
+"""add registered_date to user
+
+Revision ID: 54d08f9adc8c
+Revises: 2d472fe79b95
+Create Date: 2012-03-10 11:12:39.353857
+
+"""
+from __future__ import unicode_literals
+
+# revision identifiers, used by Alembic.
+revision = '54d08f9adc8c'
+down_revision = '2d472fe79b95'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql.base import PGDialect
+
+
+def upgrade():
+    from alembic.context import get_context
+    c = get_context()
+    if isinstance(c.connection.engine.dialect, PGDialect):
+        op.add_column('users', sa.Column('registered_date',
+                                         sa.TIMESTAMP(timezone=False),
+                                         default=sa.sql.func.now(),
+                                         server_default=sa.func.now()))
+    else:
+        op.add_column('users', sa.Column('registered_date',
+                                         sa.TIMESTAMP(timezone=False),
+                                         default=sa.sql.func.now()))
+
+
+def downgrade():
+    pass
