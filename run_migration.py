3b1482b3e61f8de2e64c336bb12d24318037323b
fmigneault <francis.charette-migneault@crim.ca>
2018-05-23 17:05:40 -0400

    adjust migration script path not found

--- run_migration.py
+++ run_migration.py
@@ -1,3 +1,6 @@
 import os
-curr_dir = os.path.dirname(__file__)+'/alembic.ini'
-os.system('alembic -c {} upgrade heads'.format(curr_dir))
+curr_dir = os.path.dirname(__file__)
+if curr_dir == '':
+    curr_dir = os.path.abspath('.')
+alembic_ini = '{}/alembic.ini'.format(curr_dir)
+os.system('alembic -c {} upgrade heads'.format(alembic_ini))
