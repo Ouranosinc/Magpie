244d3d979645789908f8a24e37c5883e4c6a430f
fmigneault <francis.charette-migneault@crim.ca>
2018-06-04 13:29:59 -0400

    add per-service resources perm mapping + db migration with resource ref to top-parent service

---
+++ magpie/alembic/versions/a395ef9d3fe6_reference_root_service.py
@@ -0,0 +1,76 @@
+"""reference root service
+
+Revision ID: a395ef9d3fe6
+Revises: ae1a3c8c7860
+Create Date: 2018-06-04 11:38:31.296950
+
+"""
+import os, sys
+cur_file = os.path.abspath(__file__)
+root_dir = os.path.dirname(cur_file)    # version
+root_dir = os.path.dirname(root_dir)    # alembic
+root_dir = os.path.dirname(root_dir)    # magpie
+root_dir = os.path.dirname(root_dir)    # root
+sys.path.insert(0, root_dir)
+
+from alembic import op
+import sqlalchemy as sa
+from alembic.context import get_context
+from sqlalchemy.dialects.postgresql.base import PGDialect
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import reflection
+from magpie import models, ANONYMOUS_USER
+from ziggurat_definitions import *
+
+Session = sessionmaker()
+
+
+# revision identifiers, used by Alembic.
+revision = 'a395ef9d3fe6'
+down_revision = 'ae1a3c8c7860'
+branch_labels = None
+depends_on = None
+
+
+def get_resource_top_parent(resource, db_session):
+    if resource is not None:
+        if resource.parent_id is None:
+            return resource
+        parent_resource = ResourceService.by_resource_id(resource.parent_id, db_session=db_session)
+        return get_resource_top_parent(parent_resource, db_session=db_session)
+    return None
+
+
+def upgrade():
+    context = get_context()
+    session = Session(bind=op.get_bind())
+
+    # two following lines avoids double 'DELETE' erroneous call when deleting group due to incorrect checks
+    # https://stackoverflow.com/questions/28824401/sqlalchemy-attempting-to-twice-delete-many-to-many-secondary-relationship
+    context.connection.engine.dialect.supports_sane_rowcount = False
+    context.connection.engine.dialect.supports_sane_multi_rowcount = False
+
+    if isinstance(context.connection.engine.dialect, PGDialect):
+
+        # check if column exists, add it otherwise
+        inspector = reflection.Inspector.from_engine(context.connection.engine)
+        has_root_service_column = False
+        for column in inspector.get_columns(table_name='resources'):
+            if 'root_service_id' in column['name']:
+                has_root_service_column = True
+                break
+
+        if not has_root_service_column:
+            op.add_column('resources', sa.Column('root_service_id', sa.Integer(), nullable=True))
+
+        # add existing resource references to their root service, loop through reference tree chain
+        all_resources = session.query(models.Resource)
+        for resource in all_resources:
+            service_resource = get_resource_top_parent(resource, session)
+            if service_resource.resource_id != resource.resource_id:
+                resource.root_service_id = service_resource.resource_id
+        session.commit()
+
+
+def downgrade():
+    pass
