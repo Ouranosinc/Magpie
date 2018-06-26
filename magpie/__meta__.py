84c923d597520e32b97a305399c3893ca3ba1329
fmigneault <francis.charette-migneault@crim.ca>
2018-06-26 19:25:10 -0400

    update history and version 0.6.1

--- magpie/__meta__.py
+++ magpie/__meta__.py
@@ -3,7 +3,7 @@ General meta information on the magpie package.
 """
 
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 __author__ = "Francois-Xavier Derue, Francis Charette Migneault"
 __email__ = 'francis.charette-migneault@crim.ca'
