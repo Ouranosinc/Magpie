99a9da1fd94e4c6f2609bf44c3749f2c5962cf2b
David Byrns <david.byrns@crim.ca>
2018-07-06 12:10:08 -0400

    Update version

--- magpie/__meta__.py
+++ magpie/__meta__.py
@@ -3,6 +3,6 @@ General meta information on the magpie package.
 """
 
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __author__ = "Francois-Xavier Derue, Francis Charette-Migneault"
 __email__ = 'francis.charette-migneault@crim.ca'
