10ea83748203c9c2ec754b81b8b75ac529dcbe31
fmigneault <francis.charette-migneault@crim.ca>
2018-07-09 15:24:52 -0400

    fix setup db session

--- magpie/adapter/__init__.py
+++ magpie/adapter/__init__.py
@@ -47,6 +47,22 @@ class MagpieAdapter(AdapterInterface):
     def owsproxy_config(self, settings, config):
         protected_path = settings.get('twitcher.ows_proxy_protected_path', '/ows')
 
+        settings = config.get_settings()
+
+        # use pyramid_tm to hook the transaction lifecycle to the request
+        config.include('pyramid_tm')
+
+        session_factory = get_session_factory(get_engine(settings))
+        config.registry['dbsession_factory'] = session_factory
+
+        # make request.dbsession available for use in Pyramid
+        config.add_request_method(
+            # r.tm is the transaction manager used by pyramid_tm
+            lambda r: get_tm_session(session_factory, r.tm),
+            'db',
+            reify=True
+        )
+
         config.add_route('owsproxy', protected_path + '/{service_name}')
         config.add_route('owsproxy_extra', protected_path + '/{service_name}/{extra_path:.*}')
         config.add_route('owsproxy_secured', protected_path + '/{service_name}/{access_token}')
