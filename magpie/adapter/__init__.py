686c7ddb8205731fbbd1e2b12adfff16a7dad907
fmigneault <francis.charette-migneault@crim.ca>
2018-07-11 14:45:41 -0400

    reuse auth config loader

--- magpie/adapter/__init__.py
+++ magpie/adapter/__init__.py
@@ -4,6 +4,7 @@ from magpie.definitions.twitcher_definitions import *
 from magpie.adapter.magpieowssecurity import *
 from magpie.adapter.magpieservice import *
 from magpie.models import get_user
+from magpie.security import auth_config_from_settings
 from magpie.db import *
 import logging
 logger = logging.getLogger(__name__)
@@ -21,27 +22,16 @@ class MagpieAdapter(AdapterInterface):
         return MagpieOWSSecurity()
 
     def configurator_factory(self, settings):
-        magpie_secret = settings['magpie.secret']
-
         # Disable rpcinterface which is conflicting with postgres db
         settings['twitcher.rpcinterface'] = False
 
-        authn_policy = AuthTktAuthenticationPolicy(
-            magpie_secret,
-            callback=groupfinder,
-        )
-        authz_policy = ACLAuthorizationPolicy()
-
-        config = Configurator(
-            settings=settings,
-            authentication_policy=authn_policy,
-            authorization_policy=authz_policy
-        )
-
+        logger.info('Loading MagpieAdapter config')
+        config = auth_config_from_settings(settings)
         config.set_request_property(get_user, 'user', reify=True)
         return config
 
     def owsproxy_config(self, settings, config):
+        logger.info('Loading MagpieAdapter owsproxy config')
         protected_path = settings.get('twitcher.ows_proxy_protected_path', '/ows')
 
         # use pyramid_tm to hook the transaction lifecycle to the request
@@ -50,7 +40,7 @@ class MagpieAdapter(AdapterInterface):
         session_factory = get_session_factory(get_engine(settings))
         config.registry['dbsession_factory'] = session_factory
 
-        # make request.dbsession available for use in Pyramid
+        # make request.db available for use in Pyramid
         config.add_request_method(
             # r.tm is the transaction manager used by pyramid_tm
             lambda r: get_tm_session(session_factory, r.tm),
@@ -58,6 +48,7 @@ class MagpieAdapter(AdapterInterface):
             reify=True
         )
 
+        logger.info('Adding MagpieAdapter owsproxy routes and views')
         config.add_route('owsproxy', protected_path + '/{service_name}')
         config.add_route('owsproxy_extra', protected_path + '/{service_name}/{extra_path:.*}')
         config.add_route('owsproxy_secured', protected_path + '/{service_name}/{access_token}')
