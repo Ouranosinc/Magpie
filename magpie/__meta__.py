6d5f8649c7c9f6926b5d4be7e63c4d8d0ccca796
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-09-13 17:54:16 -0400

    update tag 0.6.5-dev

--- magpie/__meta__.py
+++ magpie/__meta__.py
@@ -2,7 +2,7 @@
 General meta information on the magpie package.
 """
 
-__version__ = '0.6.5'
+__version__ = '0.6.5-dev'
 __author__ = "Francois-Xavier Derue, Francis Charette-Migneault"
 __maintainer__ = "Francis Charette-Migneault"
 __email__ = 'francis.charette-migneault@crim.ca'
