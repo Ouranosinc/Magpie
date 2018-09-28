6e799e1438dc8ab2486a6fd2f25150960dfb2b53
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-09-28 17:32:05 -0400

    update 'api' in db + add sync_type

--- magpie/alembic/versions/73639c63c4fc_unified_api_service.py
+++ magpie/alembic/versions/73639c63c4fc_unified_api_service.py
@@ -6,21 +6,21 @@ Create Date: 2018-09-27 16:12:02.282830
 
 """
 import os, sys
+
 cur_file = os.path.abspath(__file__)
-root_dir = os.path.dirname(cur_file)    # version
-root_dir = os.path.dirname(root_dir)    # alembic
-root_dir = os.path.dirname(root_dir)    # magpie
-root_dir = os.path.dirname(root_dir)    # root
+root_dir = os.path.dirname(cur_file)  # version
+root_dir = os.path.dirname(root_dir)  # alembic
+root_dir = os.path.dirname(root_dir)  # magpie
+root_dir = os.path.dirname(root_dir)  # root
 sys.path.insert(0, root_dir)
 
 from alembic import op
 from alembic.context import get_context
 from magpie.definitions.sqlalchemy_definitions import *
-from magpie import models
-from magpie.api.management.resource.resource_utils import get_resource_root_service
+from magpie.models import Service
+from magpie.alembic.utils import has_column
 
 Session = sessionmaker()
-
 
 # revision identifiers, used by Alembic.
 revision = '73639c63c4fc'
@@ -33,25 +33,40 @@ def upgrade():
     context = get_context()
     session = Session(bind=op.get_bind())
     if isinstance(context.connection.engine.dialect, PGDialect):
-        for service in models.Service.all(db_session=session):
-            if service.type == 'project-api':
-                service.type = 'api'
-                service.url = service.url + '/api'
-            elif service.type == 'geoserver-api':
-                service.type = 'api'
-                service.url = service.url.rstrip('/')
-    session.commit()
+        # add 'sync_type' column if missing
+        if not has_column(context, 'services', 'sync_type'):
+            op.add_column('services', sa.Column('sync_type', sa.UnicodeText(), nullable=True))
+
+        # transfer 'api' service types
+        session.query(Service). \
+            filter(Service.type == 'project-api'). \
+            update({Service.type: 'api',
+                    Service.url: Service.url + '/api',
+                    Service.sync_type: 'project-api'}, synchronize_session=False)
+        session.query(Service). \
+            filter(Service.type == 'geoserver-api'). \
+            update({Service.type: 'api',
+                    Service.sync_type: 'geoserver-api'}, synchronize_session=False)
+        session.commit()
 
 
 def downgrade():
     context = get_context()
     session = Session(bind=op.get_bind())
     if isinstance(context.connection.engine.dialect, PGDialect):
-        for service in models.Service.all(db_session=session):
-            if service.type == 'api':
-                if 'geoserver/rest' in service.url:
-                    service.type = 'geoserver-api'
-                else:
-                    service.type = 'project-api'
-                    service.url = service.url.rstrip('/api')
-    session.commit()
+        # transfer 'api' service types
+        services_project_api = session.query(Service).filter(Service.sync_type == 'project-api')
+        for svc in services_project_api:
+            svc_url = svc.url.rstrip('/api')
+            session.query(Service). \
+                filter(Service.resource_id == svc.resource_id). \
+                update({Service.type: 'project-api', Service.url: svc_url}, synchronize_session=False)
+        session.flush()
+        session.query(Service). \
+            filter(Service.sync_type == 'geoserver-api'). \
+            update({Service.type: 'geoserver-api'}, synchronize_session=False)
+        session.flush()
+        # drop 'sync_type' column
+        if has_column(context, 'services', 'sync_type'):
+            op.drop_column('services', 'sync_type')
+        session.commit()
