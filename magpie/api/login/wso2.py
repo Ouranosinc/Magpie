cf229e55c566fee79b5f0aecc437023f7978feb4
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2018-10-29 15:02:36 -0400

    debug override _fetch

--- magpie/api/login/wso2.py
+++ magpie/api/login/wso2.py
@@ -46,10 +46,92 @@ class WSO2(OAuth2):
         user.link = data.get('url')
         return user
 
+
+    # TODO: for debug, remove
     def _fetch(self, url, method='GET', params=None, headers=None, body='', max_redirects=5, content_parser=None):
         try:
-            return super(WSO2, self)._fetch(url, method=method, params=params, headers=headers, body=body,
-                                            max_redirects=max_redirects, content_parser=content_parser)
+            from six.moves.urllib import parse
+            from authomatic.six.moves import http_client
+            from authomatic.exceptions import FetchError
+            import logging
+            logger = logging.getLogger('magpie.wso2.fetch')
+
+            # 'magic' using _kwarg method
+            # pylint:disable=no-member
+            params = params or {}
+            params.update(self.access_params)
+
+            headers = headers or {}
+            headers.update(self.access_headers)
+
+            url_parsed = parse.urlsplit(url)
+            query = parse.urlencode(params)
+
+            if method in ('POST', 'PUT', 'PATCH'):
+                if not body:
+                    # Put querystring to body
+                    body = query
+                    query = ''
+                    headers.update(
+                        {'Content-Type': 'application/x-www-form-urlencoded'})
+            request_path = parse.urlunsplit(
+                ('', '', url_parsed.path or '', query or '', ''))
+
+            logger.warn(u' \u251C\u2500 host: {0}'.format(url_parsed.hostname))
+            logger.warn(u' \u251C\u2500 path: {0}'.format(request_path))
+            logger.warn(u' \u251C\u2500 method: {0}'.format(method))
+            logger.warn(u' \u251C\u2500 body: {0}'.format(body))
+            logger.warn(u' \u251C\u2500 params: {0}'.format(params))
+            logger.warn(u' \u2514\u2500 headers: {0}'.format(headers))
+
+            # Connect
+            if url_parsed.scheme.lower() == 'https':
+                connection = http_client.HTTPSConnection(
+                    url_parsed.hostname,
+                    port=url_parsed.port)
+            else:
+                connection = http_client.HTTPConnection(
+                    url_parsed.hostname,
+                    port=url_parsed.port)
+
+            try:
+                connection.request(method, request_path, body, headers)
+            except Exception as e:
+                raise FetchError('Fetching URL failed',
+                                 original_message=str(e),
+                                 url=request_path)
+
+            response = connection.getresponse()
+            location = response.getheader('Location')
+
+            if response.status in (300, 301, 302, 303, 307) and location:
+                if location == url:
+                    raise FetchError('Url redirects to itself!',
+                                     url=location,
+                                     status=response.status)
+
+                elif max_redirects > 0:
+                    remaining_redirects = max_redirects - 1
+
+                    logger.warn(u'Redirecting to {0}'.format(url))
+                    logger.warn(u'Remaining redirects: {0}'.format(remaining_redirects))
+
+                    # Call this method again.
+                    response = self._fetch(url=location,
+                                           params=params,
+                                           method=method,
+                                           headers=headers,
+                                           max_redirects=remaining_redirects)
+
+                else:
+                    raise FetchError('Max redirects reached!',
+                                     url=location,
+                                     status=response.status)
+            else:
+                logger.warn(u'Got response:')
+                logger.warn(u' \u251C\u2500 url: {0}'.format(url))
+                logger.warn(u' \u251C\u2500 status: {0}'.format(response.status))
+                logger.warn(u' \u2514\u2500 headers: {0}'.format(response.getheaders()))
         except Exception as ex:
             import logging
             import traceback
