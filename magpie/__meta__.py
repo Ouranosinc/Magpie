3a5b9797e91c6c859690969ad87371461cfac025
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2019-09-20 14:15:43 -0400

    Bump version: 1.5.0 → 1.6.0

--- magpie/__meta__.py
+++ magpie/__meta__.py
@@ -2,7 +2,7 @@
 General meta information on the magpie package.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __title__ = "Magpie"
 __package__ = "magpie"
 __author__ = "Francois-Xavier Derue, Francis Charette-Migneault"
