993a16766bc4671a1836fffd5cf5bbd2a1dfdd72
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-03-06 13:12:57 -0500

    avoid override of default magpie ini path if none provided as argument

--- magpie/app.py
+++ magpie/app.py
@@ -24,7 +24,7 @@ def main(global_config=None, **settings):  # noqa: F811
     """
     # override magpie ini if provided with --paste to gunicorn, otherwise use environment variable
     config_env = get_constant("MAGPIE_INI_FILE_PATH", raise_missing=True)
-    config_ini = (global_config or {}).get("__file__")
+    config_ini = (global_config or {}).get("__file__", config_env)
     if config_ini != config_env:
         import magpie.constants
         magpie.constants.MAGPIE_INI_FILE_PATH = config_ini
