c6bf6438bc3dd44cf3ddd93fecea72cc914f1c6f
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-09-03 17:46:49 -0400

    add supported python versions in setup & travis

--- setup.py
+++ setup.py
@@ -172,7 +172,10 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
 
     # -- Package structure -------------------------------------------------
     packages=[__meta__.__package__],
