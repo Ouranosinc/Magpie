298bd37c241c69dc843b264f56e029a52a3885ac
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-09-22 16:25:59 -0400

    adjust alembic revisions sorted by date

---
+++ magpie/alembic/versions/2020-09-22_a2a039e2cff5_explicit_permission_names.py
@@ -0,0 +1,25 @@
+"""Update permission names to explicitly define applicable access and scope.
+
+Revision ID: a2a039e2cff5
+Revises: b739afcc91db
+Create Date: 2020-09-22 14:35:04.540505
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "a2a039e2cff5"
+down_revision = "b739afcc91db"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
