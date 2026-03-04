10990536a12a2e3502a0b41732fa5980d62cfc63
Francis Charette-Migneault <francis.charette.migneault@gmail.com>
2026-03-03 21:27:51 -0500

    Refactor curl commands for better readability

--- docs/_examples/network/init.sh
+++ docs/_examples/network/init.sh
@@ -4,12 +4,30 @@ HOSTS="magpie1 magpie2 magpie3"
 
 for magpie in $HOSTS; do
     cookiejar=/tmp/"${magpie}".cookiejar
-	curl -X POST http://host.docker.internal/${magpie}/signin -H "Content-Type: application/json" -d '{"user_name": "'${MAGPIE_ADMIN_USER}'", "password": "'${MAGPIE_ADMIN_PASSWORD}'"}' --cookie-jar ${cookiejar}
+    curl \
+        --cookie-jar "${cookiejar}" \
+        -X POST "http://host.docker.internal/${magpie}/signin" \
+        -H "Content-Type: application/json" \
+        -d '{"user_name": "'${MAGPIE_ADMIN_USER}'", "password": "'${MAGPIE_ADMIN_PASSWORD}'"}'
     for user in test1 test2 test3; do
-        curl --cookie ${cookiejar} -X POST http://host.docker.internal/${magpie}/users -d user_name=${user} -d email=${user}@example.com -d password=qwertyqwerty! -d group_name=users
+        curl \
+            --cookie "${cookiejar}" \
+            -X POST "http://host.docker.internal/${magpie}/users" \
+            -d user_name="${user}" \
+            -d email="${user}@example.com" \
+            -d password='qwertyqwerty!' \
+            -d group_name="users"
     done
     for other in $HOSTS; do
         [ "$magpie" = "$other" ] && continue
-        curl --cookie ${cookiejar} -X POST http://host.docker.internal/${magpie}/network/nodes -d base_url=http://host.docker.internal/${other}/ -d name=${other} -d jwks_url=http://host.docker.internal/${other}/network/jwks -d token_url=http://host.docker.internal/${other}/network/token -d authorization_url=http://host.docker.internal/${other}/ui/network/authorize -d redirect_uris="[\"http://host.docker.internal/${other}/network/link\"]"
+        curl \
+            --cookie "${cookiejar}" \
+            -X POST "http://host.docker.internal/${magpie}/network/nodes" \
+            -d base_url="http://host.docker.internal/${other}/" \
+            -d name="${other}" \
+            -d jwks_url="http://host.docker.internal/${other}/network/jwks" \
+            -d token_url="http://host.docker.internal/${other}/network/token" \
+            -d authorization_url="http://host.docker.internal/${other}/ui/network/authorize" \
+            -d redirect_uris="[\"http://host.docker.internal/${other}/network/link\"]"
     done
 done
